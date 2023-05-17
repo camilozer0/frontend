3a576bb8b5584a6342871d4855e6561ccede1505
andres.mazorra <andres.mazorra@sophossolutions.com>
2023-05-17 14:53:09 -0500

    routerlink2.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent}
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', loadChildren: () => import('./components/dashboard/dashboard.module').then(m => m.DashboardModule) }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/dashboard/dashboard-routing.module.ts
+++ src/app/components/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { DashboardComponent } from './dashboard.component';
+import { PruebasComponent } from 'src/app/pruebas/pruebas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: DashboardComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      //{ path: 'citas', component: HomeComponent },
+      { path: 'pruebas', component: PruebasComponent },
+      //{ path: 'afiliados', component: HomeComponent },
+    ]
+  }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
