ee5478dee04df3b9350daaf0b3399f02eed66e70
andres.mazorra <andres.mazorra@sophossolutions.com>
2023-05-23 15:42:20 -0500

    editar pruebas y citas

--- src/app/components/forms/editarcita/editarcita.component.ts
+++ src/app/components/forms/editarcita/editarcita.component.ts
@@ -1,37 +1,54 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl } from '@angular/forms';
-
-
-export interface Food {
-  value: number;
-  viewValue: number;
-}
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Afiliado } from 'src/app/models/afiliado';
+import { Test } from 'src/app/models/test';
+import { AfiliadoService } from 'src/app/services/afiliado.service';
+import { PruebaService } from 'src/app/services/prueba.service';
 
 @Component({
   selector: 'app-editarcita',
   templateUrl: './editarcita.component.html',
   styleUrls: ['./editarcita.component.css']
 })
-export class EditarcitaComponent {
-
-  foods: Food[] = [
-    { value: 1, viewValue: 1 },
-    { value: 2, viewValue: 2 },
-    { value: 3, viewValue: 3 }
-  ];
+export class EditarcitaComponent implements OnInit {
 
   tituloInicial = 'Citas - Actualizar Cita';
 
+  public affOption: Afiliado[] = [];
+
+  public testOption: Test[] = [];
+
   citasForm = this.fb.group({
-    date: new FormControl(''),
-    time: new FormControl(''),
-    idTest: new FormControl(''),
-    idAffiliate: new FormControl('')
+    date: new FormControl('', Validators.required),
+    hour: new FormControl('', Validators.required),
+    idTest: new FormControl('', Validators.required),
+    idAffiliate: new FormControl('', Validators.required)
   });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+    private affService: AfiliadoService,
+    private testService: PruebaService) { }
 
-  crearCita() {
+  ngOnInit(): void {
+    this.afiliados();
+    this.tests();
+  }
+
+  afiliados() {
+    this.affService.getAfiliados().subscribe(response => {
+      this.affOption = response;
+      console.log(this.affOption)
+    });
+  }
+
+  tests() {
+    this.testService.getTests().subscribe(response => {
+      this.testOption = response;
+      console.log(this.testOption);
+    });
+  }
+
+  crearCita(dataCita: any) {
 
   }
 
